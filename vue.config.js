97f6809bdc9115c5f27dad83b1e6557c4c39b9fe
shadaozhou <841512926@qq.com>
2020-06-12 17:21:14 +0800

    s

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,8 @@ module.exports = {
         open: true, //配置自动启动浏览器
         proxy: {// 配置多个代理(配置一个 proxy: 'http://localhost:4000' )
           '/*': {
-    //         target: 'http://47.101.157.241:8090',
-            target: 'http://127.0.0.1:8090',
+            target: 'http://47.101.157.241:8090',
+    //         target: 'http://127.0.0.1:8090',
             changeOrigin: true, //允许跨域
             // pathRewrite: {       //需要rewrite重写的, 如果在服务器端做了处理则可以不要这段
             //   '^/': 'api'
