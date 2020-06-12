908e5932433f04c5cefa4bcd4f0f2f507fde023e
shadaozhou <shadaozhou@qiakr.com>
2020-06-13 02:20:10 +0800

    ss

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
         proxy: {// 配置多个代理(配置一个 proxy: 'http://localhost:4000' )
           '/*': {
     //         target: 'http://47.101.157.241:8090',
-            target: 'http://192.168.108.85:8090',
+            target: 'http://192.168.108.87:8090',
     //         target: 'http://127.0.0.1:8090',
             changeOrigin: true, //允许跨域
             pathRewrite: {       //需要rewrite重写的, 如果在服务器端做了处理则可以不要这段
